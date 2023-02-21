982d9124c3448b70e7f7f6523d746985d048cc5f
ThanuraSilva <thanurasilva1989@gmail.com>
2023-02-22 03:30:39 +0530

    Update method optimize

--- src/main/java/com/thanu/work/todo2023/service/implService/UserServiceIMPL.java
+++ src/main/java/com/thanu/work/todo2023/service/implService/UserServiceIMPL.java
@@ -36,6 +36,7 @@ public class UserServiceIMPL implements UserService {
         if(!tmpUser.isPresent()) throw new NotFoundException("User not found");
         tmpUser.get().setUserName(userDTO.getUserName());
         tmpUser.get().setPassword(userDTO.getPassword());
+        userRepository.save(entityToDTO.getUserEntity(userDTO));
     }
 
     @Override
