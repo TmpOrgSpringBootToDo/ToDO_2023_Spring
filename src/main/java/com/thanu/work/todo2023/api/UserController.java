98b704dd3f194c54f580fb7d6e502278e1badca7
ThanuraSilva <thanurasilva1989@gmail.com>
2023-02-24 14:11:19 +0530

    Minor revisions applied

--- src/main/java/com/thanu/work/todo2023/api/UserController.java
+++ src/main/java/com/thanu/work/todo2023/api/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/users")
 
